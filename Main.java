62ac54e82e77457fb117307376fdfb5ed9772418
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-04 19:33:56 -0400

    Add Main.Java

---
+++ Main.java
@@ -0,0 +1,250 @@
+
+public class Main {
+    // create a sum function
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // create an array of numbers 1 to 10
+        // create a for loop to add all the numbers in the array
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        System.out.println(sum);
+    }
+
+    // create a divide function
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    // create a multiply function
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // create a subtract function
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // create a modulus function
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+
+    // create a factorial function
+    public static int factorial(int a) {
+        int fact = 1;
+        for (int i = 1; i <= a; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    // create a fibonacci function
+    public static int fibonacci(int a) {
+        if (a == 0) {
+            return 0;
+        } else if (a == 1) {
+            return 1;
+        } else {
+            return fibonacci(a - 1) + fibonacci(a - 2);
+        }
+    }
+
+    // create a power function
+
+    public static int power(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result = result * a;
+        }
+        return result;
+    }
+
+    // create a square root function
+    public static double squareRoot(int a) {
+        double t;
+        double squareRoot = a / 2;
+        do {
+            t = squareRoot;
+            squareRoot = (t + (a / t)) / 2;
+        } while ((t - squareRoot) != 0);
+        return squareRoot;
+    }
+
+    // create a cube root function
+    public static double cubeRoot(int a) {
+        double t;
+        double cubeRoot = a / 3;
+        do {
+            t = cubeRoot;
+            cubeRoot = (2 * t + a / (t * t)) / 3;
+        } while ((t - cubeRoot) != 0);
+        return cubeRoot;
+    }
+
+    // create a absolute value function
+    public static int absoluteValue(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+
+    // create a sine function
+
+    public static double sine(int a) {
+        double sine = 0;
+        for (int i = 0; i < 10; i++) {
+            sine = sine + (power(-1, i) * power(a, 2 * i + 1)) / factorial(2 * i + 1);
+        }
+        return sine;
+    }
+
+    // create a cosine function
+    public static double cosine(int a) {
+        double cosine = 0;
+        for (int i = 0; i < 10; i++) {
+            cosine = cosine + (power(-1, i) * power(a, 2 * i)) / factorial(2 * i);
+        }
+        return cosine;
+    }
+
+    // create a tangent function
+
+    public static double tangent(int a) {
+        return sine(a) / cosine(a);
+    }
+
+    // create a log function
+
+    public static double log(int a) {
+        return Math.log(a);
+    }
+
+    // create a natural log function
+
+    public static double naturalLog(int a) {
+        return Math.log(a);
+    }
+
+    // create a log base 10 function
+
+    public static double logBase10(int a) {
+        return Math.log10(a);
+    }
+
+    // create a log base 2 function
+
+    public static double logBase2(int a) {
+        return Math.log(a) / Math.log(2);
+    }
+
+    // create a inverse sine function
+
+    public static double inverseSine(int a) {
+        return Math.asin(a);
+    }
+
+    // create a inverse cosine function
+
+    public static double inverseCosine(int a) {
+        return Math.acos(a);
+    }
+
+    // create an inverse tangent function
+
+public static double inverseTangent(int a) {
+        return Math.atan(a);
+    }
+
+    // create a inverse log function
+
+    public static double inverseLog(int a) {
+        return Math.pow(10, a);
+    }
+
+    // create a inverse natural log function
+
+    public static double inverseNaturalLog(int a) {
+        return Math.pow(Math.E, a);
+    }
+
+    // create a inverse log base 10 function
+
+    public static double inverseLogBase10(int a) {
+        return Math.pow(10, a);
+    }
+
+    // create a inverse log base 2 function
+
+    public static double inverseLogBase2(int a) {
+        return Math.pow(2, a);
+    }
+
+    // create a inverse square root function
+
+    public static double inverseSquareRoot(int a) {
+        return 1 / squareRoot(a);
+    }
+
+    // create a inverse cube root function
+
+    public static double inverseCubeRoot(int a) {
+        return 1 / cubeRoot(a);
+    }
+
+    // create a inverse power function
+
+    public static double inversePower(int a, int b) {
+        return 1 / power(a, b);
+    }
+
+    // create a inverse factorial function
+
+    public static double inverseFactorial(int a) {
+        return 1 / factorial(a);
+    }
+
+    // create a inverse fibonacci function
+
+    public static double inverseFibonacci(int a) {
+        return 1 / fibonacci(a);
+    }
+
+    // create a inverse modulus function
+
+    public static double inverseModulus(int a, int b) {
+        return 1 / modulus(a, b);
+    }
+
+    // create a inverse subtract function
+
+    public static double inverseSubtract(int a, int b) {
+        return 1 / subtract(a, b);
+    }
+
+    // create an inverse multiply function
+
+    public static double inverseMultiply(int a, int b) {
+        return 1 / multiply(a, b);
+    }
+
+    // create an inverse divide function
+
+    public static double inverseDivide(int a, int b) {
+        return 1 / divide(a, b);
+    }
+
+    // create an inverse add function
+
+
+
+
+}
